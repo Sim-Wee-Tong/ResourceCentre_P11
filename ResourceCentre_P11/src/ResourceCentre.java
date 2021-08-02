3f5466283f5bda25c17c51c52d7112c65f1cb1f3
65811 <65811@LAPTOP-QDG5FPKF>
2021-08-02 14:06:55 +0800

    update ResourceCentre

--- ResourceCentre_P11/src/ResourceCentre.java
+++ ResourceCentre_P11/src/ResourceCentre.java
@@ -47,7 +47,7 @@ public class ResourceCentre {
 				} else {
 					System.out.println("Invalid type");
 				}
-// test
+// test1
 			} else if (option == 3) {
 				// Loan item
 				ResourceCentre.setHeader("LOAN");			
